6aafaf9f3bfb5424273921968e70b10ba045a6a1
RonalsanD <gojoanap@gmail.com>
2021-10-23 13:45:48 -0500

    En la clase opciones se creo un metodo tipo arrays donde aparecen todas las elecciones de las preguntas

--- RandomQuestions/src/main/java/Opciones.java
+++ RandomQuestions/src/main/java/Opciones.java
@@ -1,11 +1,223 @@
+import java.util.ArrayList;
+
 public class Opciones {
-
-    public Opciones (){
+    ArrayList<String> opciones;
+    public Opciones(){
 
     }
-
-    public String categoria1Pregunta1(){
-        String[] Opcionespregunta1={};
-        return null;
+    public ArrayList opcionesCategoria1Pregunta1(){
+        opciones.add("Café");
+        opciones.add("Rosado");
+        opciones.add("Verde");
+        opciones.add("Violeta");
+        return opciones;
     }
+    public ArrayList opcionesCategoria1Pregunta2(){
+        opciones.add("6");
+        opciones.add("8");
+        opciones.add("10");
+        opciones.add("4");
+        return opciones;
+    }
+    public ArrayList opcionesCategoria1Pregunta3(){
+        opciones.add("El gato");
+        opciones.add("El perro");
+        opciones.add("Un pez");
+        opciones.add("Un cocodrilo");
+        return opciones;
+    }
+    public ArrayList opcionesCategoria1Pregunta4(){
+        opciones.add("La Gacela Saltarina");
+        opciones.add("El Guepardo chita");
+        opciones.add("El León");
+        opciones.add("Antílope Americano");
+        return opciones;
+    }
+    public ArrayList opcionesCategoria1Pregunta5() {
+        opciones.add("La Gacela Saltarina");
+        opciones.add("El Guepardo chita");
+        opciones.add("El León");
+        opciones.add("Antílope Americano");
+        return opciones;
+    }
+    public ArrayList opcionesCategoria2Pregunta1(){
+        opciones.add("Pacifico");
+        opciones.add("Atlántico");
+        opciones.add("Ártico");
+        opciones.add("Antártico");
+        return opciones;
+    }
+    public ArrayList opcionesCategoria2Pregunta2(){
+        opciones.add("El resultado es 45");
+        opciones.add("El resultado es 30");
+        opciones.add("El resultado es 38");
+        opciones.add("El resultado es 49");
+        return opciones;
+    }
+    public ArrayList opcionesCategoria2Pregunta3(){
+        opciones.add("Un cántate canadiense");
+        opciones.add("Un actor alemán");
+        opciones.add("Un científico estadounidense");
+        opciones.add("El Líder de la iglesia católica");
+        return opciones;
+    }
+    public ArrayList opcionesCategoria2Pregunta4(){
+        opciones.add("Tierra");
+        opciones.add("Mercurio");
+        opciones.add("Marte");
+        opciones.add("Júpiter");
+        return opciones;
+    }
+    public ArrayList opcionesCategoria2Pregunta5() {
+        opciones.add("África");
+        opciones.add("Asia");
+        opciones.add("Europa");
+        opciones.add("Oceanía");
+        return opciones;
+    }
+    public ArrayList opcionesCategoria3Pregunta1(){
+        opciones.add("Alemania");
+        opciones.add("Austria");
+        opciones.add("Berlin");
+        opciones.add("Russia");
+        return opciones;
+    }
+    public ArrayList opcionesCategoria3Pregunta2(){
+        opciones.add("5 años");
+        opciones.add("10 años");
+        opciones.add("50 años");
+        opciones.add("15 años");
+        return opciones;
+    }
+    public ArrayList opcionesCategoria3Pregunta3(){
+        opciones.add("El Señor de los Anillos");
+        opciones.add("Don Quijote de la Mancha");
+        opciones.add("La Biblia");
+        opciones.add("El Principito");
+        return opciones;
+    }
+    public ArrayList opcionesCategoria3Pregunta4(){
+        opciones.add("Gaélico");
+        opciones.add("Hebreo");
+        opciones.add("Euskera");
+        opciones.add("Latín");
+        return opciones;
+    }
+    public ArrayList opcionesCategoria3Pregunta5() {
+        opciones.add("Carnívoro");
+        opciones.add("Omnívoro");
+        opciones.add("Herbívoro");
+        opciones.add("Insectívoro");
+        return opciones;
+    }
+    public ArrayList opcionesCategoria4Pregunta1(){
+        opciones.add("Australia");
+        opciones.add("Egipto");
+        opciones.add("Siria");
+        opciones.add("China");
+        return opciones;
+    }
+    public ArrayList opcionesCategoria4Pregunta2(){
+        opciones.add("Tortuga de las Galápagos");
+        opciones.add("Las Esponjas Marinas");
+        opciones.add("Almeja de Islandia");
+        opciones.add("Medusa Turritopsis nutricula");
+        return opciones;
+    }
+    public ArrayList opcionesCategoria4Pregunta3(){
+        opciones.add("Vallisoletano");
+        opciones.add("Valluno");
+        opciones.add("Vallaloid");
+        opciones.add("Vallesto");
+        return opciones;
+    }
+    public ArrayList opcionesCategoria4Pregunta4(){
+        opciones.add("Universidad de Bolonia");
+        opciones.add("Universidad de Oxford");
+        opciones.add("Universidad de Cambriege");
+        opciones.add("Universidad de Salamanca");
+        return opciones;
+    }
+    public ArrayList opcionesCategoria4Pregunta5() {
+        opciones.add("Equina");
+        opciones.add("Castores");
+        opciones.add("Delfines");
+        opciones.add("Armadillo");
+        return opciones;
+    }
+    public ArrayList opcionesCategoria5Pregunta1(){
+        opciones.add("El lago Titicaca");
+        opciones.add("El lago Victoria");
+        opciones.add("El lago Baikal");
+        opciones.add("El Lago Tanganica ");
+        return opciones;
+    }
+    public ArrayList opcionesCategoria5Pregunta2(){
+        opciones.add("Reactivo de Tollens");
+        opciones.add("Agua Regia");
+        opciones.add("Las sales de permagrado");
+        opciones.add("Acido clorhidrico");
+        return opciones;
+    }
+    public ArrayList opcionesCategoria5Pregunta3(){
+        opciones.add("El Basic");
+        opciones.add("El Cobol");
+        opciones.add("El C++");
+        opciones.add("El FORTRAN");
+        return opciones;
+    }
+    public ArrayList opcionesCategoria5Pregunta4(){
+        opciones.add("Júpiter");
+        opciones.add("Saturno");
+        opciones.add("Marte");
+        opciones.add("Neptuno");
+        return opciones;
+    }
+    public ArrayList opcionesCategoria5Pregunta5() {
+        opciones.add("Israel");
+        opciones.add("India");
+        opciones.add("Pakistán");
+        opciones.add("Mongolia");
+        return opciones;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
